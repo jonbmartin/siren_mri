scaling wrong variable

experiment_scripts/test_mri_neural_process.py
@@ -115,10 +115,10 @@ for i in np.linspace(0, 1, 8):
     embedding = i * embedding_1 + (1. - i) * embedding_2
     model_input = {'coords': dataio.get_mgrid(image_resolution)[None, :].cuda(), 'embedding': embedding}
     model_output = model(model_input)
-    model_output *=1000
 
     out_img = dataio.lin2img(model_output['model_out'], image_resolution).squeeze().permute(1,
                                                                                             0).detach().cpu().numpy()
+    out_img *=1000
     out_img += 1
     out_img /= 2.
     out_img = np.clip(out_img, 0., 1.)