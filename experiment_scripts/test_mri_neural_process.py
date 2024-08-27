5f9666bb9dde01ee9f6a93cbb0b7544fb73e6760
jonbmartin <jon.bach.martin@gmail.com>
2024-08-27 10:08:11 -0500

    update dimension of output reshape

--- experiment_scripts/test_mri_neural_process.py
+++ experiment_scripts/test_mri_neural_process.py
@@ -72,7 +72,7 @@ model_input = {'coords': dataio.get_mgrid(image_resolution)[None, :].cuda(),
                'coords_sub': generalization_dataset_train[0][0]['coords_sub'].unsqueeze(0).cuda()}
 model_output = model(model_input)
 
-out_img = dataio.lin2img(model_output['model_out'], image_resolution).squeeze().permute(1, 2, 0).detach().cpu().numpy()
+out_img = dataio.lin2img(model_output['model_out'], image_resolution).squeeze().permute(1, 0).detach().cpu().numpy()
 out_img += 1
 out_img /= 2.
 out_img = np.clip(out_img, 0., 1.)
