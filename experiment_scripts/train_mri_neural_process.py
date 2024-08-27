a8e5011444d604667188159d65dab93005d0cc75
jonbmartin <jon.bach.martin@gmail.com>
2024-08-27 13:11:11 -0500

    larger matrix size

--- experiment_scripts/train_mri_neural_process.py
+++ experiment_scripts/train_mri_neural_process.py
@@ -50,7 +50,7 @@ assert opt.dataset == 'mri_image'
 if opt.conv_encoder: gmode = 'conv_cnp'
 else: gmode = 'cnp'
 
-image_resolution = (32, 32)
+image_resolution = (128, 128)
 
 img_dataset = dataio.FastMRIBrain(split='train', downsampled=True, image_resolution=image_resolution)
 #img_dataset = dataio.MRIImageDomain(split='train',downsample=True)
