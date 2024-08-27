db7a0078abfec557917c0b09602bdb49f5e82650
jonbmartin <jon.bach.martin@gmail.com>
2024-08-27 10:18:36 -0500

    update scaling

--- experiment_scripts/test_mri_neural_process.py
+++ experiment_scripts/test_mri_neural_process.py
@@ -115,7 +115,7 @@ for i in np.linspace(0, 1, 8):
     embedding = i * embedding_1 + (1. - i) * embedding_2
     model_input = {'coords': dataio.get_mgrid(image_resolution)[None, :].cuda(), 'embedding': embedding}
     model_output = model(model_input)
-    print(model_output)
+    model_output *=1000
 
     out_img = dataio.lin2img(model_output['model_out'], image_resolution).squeeze().permute(1,
                                                                                             0).detach().cpu().numpy()
