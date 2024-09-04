8b0878f2fdbeb2ad5ef90e89846532776cf9c2c4
jonbmartin <jon.bach.martin@gmail.com>
2024-09-04 13:52:37 -0500

    get rid of sparse imager

--- experiment_scripts/test_mri_neural_process_kspace.py
+++ experiment_scripts/test_mri_neural_process_kspace.py
@@ -175,16 +175,16 @@ def getTestMSE(dataloader, subdir):
             out_img = np.clip(out_img, 0., 1.)
             gt_img = np.clip(gt_img, 0., 1.)
 
-            sparse_img = np.ones((image_resolution[0], image_resolution[1], 1))
-            coords_sub = model_input['coords_sub'].squeeze().detach().cpu().numpy()
-            rgb_sub = model_input['img_sub'].squeeze().detach().cpu().numpy()
-            print(f'RGB sub: {np.shape(rgb_sub)}')
-            for index in range(0, coords_sub.shape[0]):
-                r = int(round((coords_sub[index][0] + 1) / 2 * (image_resolution[0]-1)))
-                c = int(round((coords_sub[index][1] + 1) / 2 * (image_resolution[1]-1)))
-                sparse_img[r, c, :] = np.clip((rgb_sub[index] + 1) / 2, 0., 1.)
+            #sparse_img = np.ones((image_resolution[0], image_resolution[1], 1))
+            #coords_sub = model_input['coords_sub'].squeeze().detach().cpu().numpy()
+            #rgb_sub = model_input['img_sub'].squeeze().detach().cpu().numpy()
+            #print(f'RGB sub: {np.shape(rgb_sub)}')
+            #for index in range(0, coords_sub.shape[0]):
+            #    r = int(round((coords_sub[index][0] + 1) / 2 * (image_resolution[0]-1)))
+            #    c = int(round((coords_sub[index][1] + 1) / 2 * (image_resolution[1]-1)))
+            #    sparse_img[r, c, :] = np.clip((rgb_sub[index] + 1) / 2, 0., 1.)
 
-            sio.savemat(os.path.join(root_path, f'ground_truth_img_{sparsity}.mat'),{'gt_img':gt_img, 'pred_img':out_img, 'sparse_img':sparse_img})
+            sio.savemat(os.path.join(root_path, f'ground_truth_img_{sparsity}.mat'),{'gt_img':gt_img, 'pred_img':out_img})
 
             #imageio.imwrite(os.path.join(root_path, subdir, str(total_steps) + '_sparse.png'), to_uint8(sparse_img))
             #imageio.imwrite(os.path.join(root_path, subdir, str(total_steps) + '.png'), to_uint8(out_img))
