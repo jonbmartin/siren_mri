7e4eb80f4bb13ea5c40eef6380f5c89aba6dccb5
jonbmartin <jon.bach.martin@gmail.com>
2024-09-04 11:35:09 -0500

    update with fourier features

--- meta_modules.py
+++ meta_modules.py
@@ -102,7 +102,7 @@ class ConvolutionalNeuralProcessImplicit2DHypernet(nn.Module):
         else:
             self.encoder = modules.ConvImgEncoder(channel=in_features, image_resolution=image_resolution)
         self.hypo_net = modules.SingleBVPNet(out_features=out_features, type='sine', sidelength=image_resolution,
-                                             in_features=2)
+                                             in_features=512)
         self.hyper_net = HyperNetwork(hyper_in_features=latent_dim, hyper_hidden_layers=1, hyper_hidden_features=256,
                                       hypo_module=self.hypo_net)
         print(self)
