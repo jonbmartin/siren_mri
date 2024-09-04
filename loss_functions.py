2f0303a60811feb6872c00e5ce735db13aca17d4
jonbmartin <jon.bach.martin@gmail.com>
2024-09-04 16:39:23 -0500

    print statement

--- loss_functions.py
+++ loss_functions.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def image_mse(mask, model_output, gt):
-    #print(np.shape(model_output['model_out']))
+    print(np.shape(model_output['model_out']))
     if mask is None:
         return {'img_loss': ((model_output['model_out'] - gt['img']) ** 2).mean()}
     else:
