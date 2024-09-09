17085461c534c9d5d29d90ef8a48156c81fc15e7
jonbmartin <jon.bach.martin@gmail.com>
2024-09-09 12:58:54 -0500

    increase hypernetwork size

--- meta_modules.py
+++ meta_modules.py
@@ -102,10 +102,10 @@ class ConvolutionalNeuralProcessImplicit2DHypernet(nn.Module):
         else:
             self.encoder = modules.ConvImgEncoder(channel=in_features, image_resolution=image_resolution)
         self.hypo_net = modules.SingleBVPNet(out_features=out_features, type='sine', sidelength=image_resolution,
-                                             in_features=2) 
+                                             in_features=2)
         
         # JBM changed number of hyper hidden layers to 3
-        self.hyper_net = HyperNetwork(hyper_in_features=latent_dim, hyper_hidden_layers=1, hyper_hidden_features=256,
+        self.hyper_net = HyperNetwork(hyper_in_features=latent_dim, hyper_hidden_layers=2, hyper_hidden_features=256,
                                       hypo_module=self.hypo_net)
         print(self)
 
