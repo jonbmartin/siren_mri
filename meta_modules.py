5009724419fecc4f694e68ef0d47e1b690b382ed
jonbmartin <jon.bach.martin@gmail.com>
2024-09-10 11:26:10 -0500

    try different size, closer to orig

--- meta_modules.py
+++ meta_modules.py
@@ -180,10 +180,11 @@ class ConvolutionalNeuralProcessImplicit2DHypernetFourierFeatures(nn.Module):
             self.encoder = modules.PartialConvImgEncoder(channel=2, image_resolution=image_resolution)
         else:
             # NOTE: our SIREN will use Fourier feat as input, BUT our encoder will use x,y image! 
+            # This may not be optimal! But simple for now...
             self.encoder = modules.ConvImgEncoder(channel=2, image_resolution=image_resolution)
         self.hypo_net = modules.SingleBVPNet(out_features=out_features, type='sine', sidelength=image_resolution,
                                              in_features=fourier_features_size) # JBM USED TO BE 2 input
-        self.hyper_net = HyperNetwork(hyper_in_features=latent_dim, hyper_hidden_layers=1, hyper_hidden_features=512,
+        self.hyper_net = HyperNetwork(hyper_in_features=latent_dim, hyper_hidden_layers=1, hyper_hidden_features=256,
                                       hypo_module=self.hypo_net)
         print(self)
 
