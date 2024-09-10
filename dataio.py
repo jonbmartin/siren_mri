35a4007b6f40dc5dc58178af66a6ae89fbcf398a
jonbmartin <jon.bach.martin@gmail.com>
2024-09-10 15:54:09 -0500

    udpate

--- dataio.py
+++ dataio.py
@@ -941,10 +941,11 @@ class ImageGeneralizationWrapper(torch.utils.data.Dataset):
         gt_dict['dc_mask'] = in_dict['dc_mask']
         #  convert to device: 
         for key, value in gt_dict.items():
-            if key != 'idx':
+            print(key)
+            if key not in {'idx'}:
                 gt_dict[key] = gt_dict[key].to(self.device)
         for key, value in in_dict.items():
-            if key != 'idk':
+            if key not in {'idk'}:
                 in_dict[key] = in_dict[key].to(self.device)
 
         return in_dict, gt_dict
