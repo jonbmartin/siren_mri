c581ede0233df05ce691ca9f5729d386a010de59
jonbmartin <jon.bach.martin@gmail.com>
2024-09-23 11:13:28 -0500

    update with smallest possible dataset

--- experiment_scripts/hyperopt_mri_neural_process.py
+++ experiment_scripts/hyperopt_mri_neural_process.py
@@ -19,8 +19,8 @@ def objective(trial):
 
     # fixed parameters
     n_trials = 1
-    batch_size = 4 # with accumulation steps =16, this is an effective batch size of 64
-    accumulation_steps = 16
+    batch_size = 2 # with accumulation steps =16, this is an effective batch size of 64
+    accumulation_steps = 32
     device = torch.device('cuda:4')  # or whatever device/cpu you like
     image_resolution = (64, 64)
     train_sparsity_range = [2000, 4000] # this gets overwritten
