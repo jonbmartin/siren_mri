ed451fe4d7795340bab79760c0a09769d762cd77
jonbmartin <jon.bach.martin@gmail.com>
2024-09-24 10:35:13 -0500

    update chosen loss

--- experiment_scripts/train_mri_neural_process.py
+++ experiment_scripts/train_mri_neural_process.py
@@ -165,8 +165,8 @@ else:
 model.to(device)
 
 # Define the loss
-loss_fn = partial(loss_functions.image_hypernetwork_log_loss, None, kl_weight, fw_weight)
-#loss_fn = partial(loss_functions.image_hypernetwork_loss, None, kl_weight, fw_weight)
+#loss_fn = partial(loss_functions.image_hypernetwork_log_loss, None, kl_weight, fw_weight)
+loss_fn = partial(loss_functions.image_hypernetwork_loss, None, kl_weight, fw_weight)
 #loss_fn = partial(loss_functions.image_hypernetwork_ift_loss, None, kl_weight, fw_weight)
 summary_fn = partial(utils.write_image_summary_small, image_resolution, None)
 
