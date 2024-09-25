e2ea44773ffa5c06af235af3a30562a4bd9145b3
jonbmartin <jon.bach.martin@gmail.com>
2024-09-25 15:39:12 -0500

    debug print

--- loss_functions.py
+++ loss_functions.py
@@ -30,6 +30,10 @@ def image_mse_log(mask, model_output, gt):
     # add a kspace domain loss:
     mag_weight = 0.0001
     phase_weight = 0.0001
+    test_mag_weight = (mag_weight *(kspace_pred_log-kspace_gt_log)**2).sum()
+    test_phase_weight = (phase_weight * (2-torch.cos(kspace_output_phase-kspace_gt_phase))).sum()
+
+    print(f'Calculated loss of mag: {test_mag_weight}, of phase: {test_phase_weight}')
     kspace_loss =  (mag_weight *(kspace_pred_log-kspace_gt_log)**2 + 
                     phase_weight * (2-torch.cos(kspace_output_phase-kspace_gt_phase))).sum()
 
