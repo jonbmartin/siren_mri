dea2376266a2d5aa17476fb08d1b09997a59e8f6
jonbmartin <jon.bach.martin@gmail.com>
2024-09-25 15:42:48 -0500

    try an l1 weight

--- loss_functions.py
+++ loss_functions.py
@@ -84,6 +84,28 @@ def image_mse(mask, model_output, gt):
         return {'img_loss': (l1_cost + kspace_loss)}
     else:
         return {'img_loss': (l1_cost + kspace_loss)}
+    
+def image_l1(mask, model_output, gt):
+    # SAME as below, but no image domain loss/ fourier transforms 
+
+    kspace_output_real = dataio.lin2img(model_output['model_out'])
+    kspace_output = kspace_output_real[:,0,:,:] + 1j * kspace_output_real[:,1,:,:]
+
+    kspace_gt_real = dataio.lin2img(gt['img'])
+
+    # add l1 reg in kspace dim to encourage sparsity
+    l1_reg = 0
+    l1_cost = l1_reg * torch.abs(kspace_output).sum()
+
+    # add a kspace domain loss:
+    kspace_weight = 0.0025
+    kspace_loss = kspace_weight * (torch.abs(kspace_output_real-kspace_gt_real)).sum()
+
+
+    if mask is None:
+        return {'img_loss': (l1_cost + kspace_loss)}
+    else:
+        return {'img_loss': (l1_cost + kspace_loss)}
 
 def ift_image_mse(mask, model_output, gt):
     
