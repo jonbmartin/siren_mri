1af4ef29df45a1fa36cbef0f1637c050aa94c580
jonbmartin <jon.bach.martin@gmail.com>
2024-09-26 15:40:07 -0500

    add additional inputs to network setup

--- meta_modules.py
+++ meta_modules.py
@@ -175,7 +175,7 @@ class ConvolutionalNeuralProcessImplicit2DHypernet(nn.Module):
 class ConvolutionalNeuralProcessImplicit2DHypernetFourierFeatures(nn.Module):
     def __init__(self, in_features, out_features, image_resolution=None, partial_conv=False, 
                  fourier_features_size=512, latent_dim=256, hidden_features=256, 
-                 num_hidden_layers=5, hyper_hidden_features=512, hyper_hidden_layers=1, device='cuda:0'):
+                 num_hidden_layers=5, hyper_hidden_features=512, hyper_hidden_layers=1, conv_kernel_size=3, device='cuda:0'):
         super().__init__()
 
         self.dc = data_consistency.DataConsistencyInKspace(noise_lvl=None)
@@ -183,7 +183,7 @@ class ConvolutionalNeuralProcessImplicit2DHypernetFourierFeatures(nn.Module):
         if partial_conv:
             self.encoder = modules.PartialConvImgEncoder(channel=2, image_resolution=image_resolution)
         else:
-            self.encoder = modules.ConvImgEncoder(channel=2, image_resolution=image_resolution, hidden_size=latent_dim)
+            self.encoder = modules.ConvImgEncoder(channel=2, image_resolution=image_resolution, hidden_size=latent_dim, kernel_size=conv_kernel_size)
         self.hypo_net = modules.SingleBVPNet(out_features=out_features, type='sine', sidelength=image_resolution,
                                              in_features=fourier_features_size, hidden_features=hidden_features,num_hidden_layers=num_hidden_layers) # JBM USED TO BE 3 layer, 128 input. good perf with 5
         self.hyper_net = HyperNetwork(hyper_in_features=latent_dim, hyper_hidden_layers=hyper_hidden_layers, hyper_hidden_features=hyper_hidden_features, # JBM used to be 256 hyperhidden
