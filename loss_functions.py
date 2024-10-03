87447a38dbbf9d26422e030c6abbf4244b061383
jonbmartin <jon.bach.martin@gmail.com>
2024-10-02 22:23:13 -0500

    update

--- loss_functions.py
+++ loss_functions.py
@@ -99,7 +99,8 @@ def image_smape(mask, model_output, gt):
 
     # add a kspace domain loss:
     kspace_weight = 1/(128*128)
-    kspace_loss = kspace_weight * ((torch.abs(kspace_output_real-kspace_gt_real))/(torch.abs(kspace_gt_real)+torch.abs(kspace_output_real))/2).sum()
+    eps = 1e-3
+    kspace_loss = kspace_weight * ((torch.abs(kspace_output_real-kspace_gt_real))/(eps + torch.abs(kspace_gt_real)+torch.abs(kspace_output_real))).sum()
 
     if mask is None:
         return {'img_loss': (kspace_loss)}
