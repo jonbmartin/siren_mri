2d803580b32eed169039c03b15bc9271e790f35f
jonbmartin <jon.bach.martin@gmail.com>
2024-10-04 23:25:41 -0500

    asinh=1

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
     fourier_features_scale = 21
     device = 1
     resume_from_save = False
-    experiment_name = 'DDP_asinh_tx_12scale'
+    experiment_name = 'DDP_asinh_tx_1scale'
 
     if resume_from_save:
         load_from_checkpoint_path = './logs/DDP/checkpoints/model_epoch_0030.pth'
