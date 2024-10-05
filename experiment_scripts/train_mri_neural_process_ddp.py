39cefa9534170fba61ccb6abf4b6a1c28c9d946a
jonbmartin <jon.bach.martin@gmail.com>
2024-10-05 17:06:23 -0500

    2000 scale

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":
     fourier_features_scale = 8
     device = 1
     resume_from_save = False
-    experiment_name = 'DDP_asinh_tx_1000scale'
+    experiment_name = 'DDP_asinh_tx_2000scale'
 
     if resume_from_save:
         load_from_checkpoint_path = './logs/DDP/checkpoints/model_epoch_0030.pth'
