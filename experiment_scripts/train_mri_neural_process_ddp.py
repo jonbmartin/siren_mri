2ea4aa67ee5376df6e15482e272722a625753f84
jonbmartin <jon.bach.martin@gmail.com>
2024-10-06 20:50:22 -0500

    update file name

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -246,7 +246,7 @@ if __name__ == "__main__":
     fourier_features_scale = 24.4
     device = 1
     resume_from_save = False
-    experiment_name = 'DDP_asinh_tx_40scale_hypopt'
+    experiment_name = 'DDP_asinh_tx_40scale_hand_tuned'
 
     if resume_from_save:
         load_from_checkpoint_path = './logs/DDP/checkpoints/model_epoch_0030.pth'
