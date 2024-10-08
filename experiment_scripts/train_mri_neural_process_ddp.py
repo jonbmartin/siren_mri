47d0fc646e31b0e077a8f3ed8ec25ccc33f3943f
jonbmartin <jon.bach.martin@gmail.com>
2024-10-08 14:07:07 -0500

    update hyperopt

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -114,7 +114,7 @@ def main(rank, world_size, total_epochs, save_every, load_from_checkpoint_path, 
     elif config =='hyperoptIV_homebrew':
         num_fourier_features = 60
         kl_weight = 2.78e-8
-        fw_weight = 6.4e-5 # JBM was e-5
+        fw_weight = 6.4e-6 # JBM was e-5
         lr = 5.57e-5 # JBM was e-5 
         fourier_features_scale = 21
         latent_dim = 128
