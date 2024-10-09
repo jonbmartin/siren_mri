2b5364695b3d3e27d1eaf8fb5ab92059f1b94cee
jonbmartin <jon.bach.martin@gmail.com>
2024-10-09 10:07:14 -0500

    update

--- meta_modules.py
+++ meta_modules.py
@@ -200,10 +200,12 @@ class ConvolutionalNeuralProcessImplicit2DHypernetFourierFeatures(nn.Module):
             # image domain embedding: 
             img_complex = model_input['img_sparse']
             print(np.shape(img_complex))
-            img_complex = img_complex[0,:,:] + 1j* img_complex[1,:,:]
+            img_complex = img_complex[:,0,:,:] + 1j* img_complex[:,1,:,:]
             img_complex = torch.fft.ifft2(img_complex)
+            img_complex = torch.stack((torch.real(img_complex),torch.imag(img_complex)),dim=1)
             print(np.shape(img_complex))
-            embedding = self.encoder(model_input['img_sparse'])
+            #embedding = self.encoder(model_input['img_sparse'])
+            embedding = self.encoder(img_complex)
         else:
             embedding = model_input['embedding']
         hypo_params = self.hyper_net(embedding)
