80c10ba0473f372776c7e456254b39803b167cf5
jonbmartin <jon.bach.martin@gmail.com>
2024-10-09 11:49:47 -0500

    update

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -273,7 +273,7 @@ if __name__ == "__main__":
 
     # TODO: manually setting this to be the same as that inside main()
     # create the fourier feature transform to be used by ALL DDP processes 
-    num_fourier_features = 60
+    num_fourier_features = 16
     fourier_features_scale = 21
     device = 1
     resume_from_save = False
