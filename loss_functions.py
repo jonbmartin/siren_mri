18ba4c7fda04bc123101e293db7d8f2e156757c3
jonbmartin <jon.bach.martin@gmail.com>
2024-10-09 10:40:55 -0500

    try l1

--- loss_functions.py
+++ loss_functions.py
@@ -88,7 +88,7 @@ def image_mse(mask, model_output, gt, high_freq=True):
     kspace_weight = 1/(128*128) # if using 3, 0.0025. If using 6, 0.02 # dim sizekspace_pred
 
     #kspace_loss = ((torch.real(cplx_diff))**2).sum() + ((torch.imag(cplx_diff))**2).sum()
-    kspace_loss = (torch.abs(high_freq_mask*(kspace_output_real-kspace_gt_real))**2).sum()
+    kspace_loss = (torch.abs(high_freq_mask*(kspace_output_real-kspace_gt_real))).sum()
     kspace_loss = kspace_loss * kspace_weight
     if mask is None:
         return {'img_loss': (kspace_loss)}
