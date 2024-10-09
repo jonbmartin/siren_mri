4af0e4b0e0a803e2bdb6109dbafddb9e888fe38a
jonbmartin <jon.bach.martin@gmail.com>
2024-10-09 16:54:17 -0500

    increase batch size

--- experiment_scripts/hyperopt_mri_neural_process.py
+++ experiment_scripts/hyperopt_mri_neural_process.py
@@ -19,7 +19,7 @@ def objective(trial, device_id):
 
     # fixed parameters
     n_trials = 1
-    batch_size = 4 # with accumulation steps =16, this is an effective batch size of 64
+    batch_size = 32 # with accumulation steps =16, this is an effective batch size of 64
     device = torch.device(device_id)  # or whatever device/cpu you like
     image_resolution = (128, 128)
     train_sparsity_range = [2000, 4000] # this gets overwritten
@@ -53,7 +53,7 @@ def objective(trial, device_id):
     num_conv_res_blocks=4
     w0 = trial.suggest_float('w0',1,100)
     #accumulation_steps = trial.suggest_int('accumulation_steps', 8, 128)
-    accumulation_steps=32
+    accumulation_steps=4
 
     
     img_dataset = dataio.FastMRIBrainKspace(split='train', downsampled=True, image_resolution=image_resolution)
@@ -130,7 +130,7 @@ def objective(trial, device_id):
 if __name__ == "__main__":
     study = optuna.load_study(
         storage = "sqlite:///db.sqlite3_test",
-        study_name = 'hyperopt_reg_params')
+        study_name = 'hyperopt_reg_params_trial2')
     
     p = configargparse.ArgumentParser()
     p.add('-d', '--device_id', required=True, help='CUDA device ID.')
