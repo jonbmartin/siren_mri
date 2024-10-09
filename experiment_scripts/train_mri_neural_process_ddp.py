6b34bdc7962b6c70a1475735ab8e4c3dac34d332
jonbmartin <jon.bach.martin@gmail.com>
2024-10-09 11:26:13 -0500

    revert hypernetwork size

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -119,7 +119,7 @@ def main(rank, world_size, total_epochs, save_every, load_from_checkpoint_path, 
         fourier_features_scale = 21
         latent_dim = 128
         hidden_features_hyper = 128
-        hidden_layers_hyper = 1
+        hidden_layers_hyper = 2
         hidden_layers = 3 # was 1
         hidden_features = 256
         partial_conv=False
