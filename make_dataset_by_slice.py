d420a62179b9dd677f421e3064fc121abf652c1c
jonbmartin <jon.bach.martin@gmail.com>
2024-10-10 16:37:22 -0500

    update

--- make_dataset_by_slice.py
+++ make_dataset_by_slice.py
@@ -21,7 +21,7 @@ root = '../fastMRIdata/' + dir
 filenames = os.listdir(root)
 
 for ii in range(len(filenames)):
-    f = h5py.File(root + filenames[ii], "w")
+    f = h5py.File(root + filenames[ii], "r")
     
     # return data as numpy array
     data = f['reconstruction'][()]
@@ -30,7 +30,7 @@ for ii in range(len(filenames)):
     slices, width, height = np.shape(data)
 
     for ss in range(slices):
-        with h5py.File(newdir+'slice_'+str(ss)+"_"+filenames[ii]) as f:
+        with h5py.File(newdir+'slice_'+str(ss)+"_"+filenames[ii], 'w') as f:
             data = f.create_dataset("reconstruction",data[ss,:,:], dtype='float32')
             f.close()
 
