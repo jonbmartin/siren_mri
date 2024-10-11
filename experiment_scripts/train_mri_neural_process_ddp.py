a3c65ca89eeb1ecead4654dac363f666ff091eab
jonbmartin <jon.bach.martin@gmail.com>
2024-10-11 11:37:36 -0500

    update

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -285,6 +285,7 @@ if __name__ == "__main__":
         print(f'Resuming training from checkpoint found at: {load_from_checkpoint_path}')
     else:
         load_from_checkpoint_path = None
+        print('Initializing ONE B feature encoding matrix')
         fourier_transformer = GaussianFourierFeatureTransform(num_input_channels=2, mapping_size_spatial=num_fourier_features, 
                                                         scale=fourier_features_scale, device=device)
         
