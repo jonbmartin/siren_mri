1035fbd069c00f076cfb5c78fed4728d7169d70c
jonbmartin <jon.bach.martin@gmail.com>
2024-10-14 11:51:25 -0500

    new way of saving

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -290,7 +290,8 @@ if __name__ == "__main__":
         print('Initializing ONE B feature encoding matrix')
         fourier_transformer = GaussianFourierFeatureTransform(num_input_channels=2, mapping_size_spatial=num_fourier_features, 
                                                         scale=fourier_features_scale, device=device)
-        fourier_transformer.save_B('./logs/'+experiment_name+'/current_B_DDP_placeholder.pt')
+        os.mkdir('./logs/'+experiment_name+'/feature_matrix')
+        fourier_transformer.save_B('./logs/'+experiment_name+'/feature_matrix/current_B_DDP_placeholder.pt')
 
         # Record the fourier feature transform matrix and place in current experiment folder.
         # Making multiple copies for the different processes
