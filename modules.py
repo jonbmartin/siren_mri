527f44a0768c8b7472ceb0d89cb1ff5e2f19060a
jonbmartin <jon.bach.martin@gmail.com>
2024-10-16 15:13:16 -0500

    change all the way with activations

--- modules.py
+++ modules.py
@@ -51,7 +51,7 @@ class FCBlock(MetaModule):
         # Dictionary that maps nonlinearity name to the respective function, initialization, and, if applicable,
         # special first-layer initialization scheme
         # TODO: changed sine init to xavier init
-        nls_and_inits = {'sine':(Sine(w0=w0), init_weights_xavier, first_layer_sine_init),
+        nls_and_inits = {'sine':(Sine(w0=w0), init_weights_xavier, None),
                          'relu':(nn.ReLU(inplace=True), init_weights_normal, None),
                          'sigmoid':(nn.Sigmoid(), init_weights_xavier, None),
                          'tanh':(nn.Tanh(), init_weights_xavier, None),
