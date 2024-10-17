d59d925ffa9b63941cb04b35f914489f73759cb6
jonbmartin <jon.bach.martin@gmail.com>
2024-10-17 16:13:39 -0500

    increase the w0 of the siren

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -163,7 +163,7 @@ def main(rank, world_size, total_epochs, save_every, load_from_checkpoint_path, 
         kl_weight = 2.78e-8 #optim # 0.1 in paper
         fw_weight = 1e-6#optim # 100 in paper
         lr = 5.e-5
-        fourier_features_scale = 20 # best = 10
+        fourier_features_scale = 16 # best = 10
         latent_dim = 256 # best = 256
         hidden_features_hyper = 128 #256 # best = 256
         hidden_layers_hyper = 5 # try just 1. 3 gave 0.0011 after 15 epochs. 5 gave 0.0004!! 
@@ -172,7 +172,7 @@ def main(rank, world_size, total_epochs, save_every, load_from_checkpoint_path, 
         partial_conv=False
         conv_kernel_size = 5
         num_conv_res_blocks= 3 # go back to orig paper, was 3
-        w0=30
+        w0=50
 
     image_resolution = (128, 128)
     use_fourier_features = True
