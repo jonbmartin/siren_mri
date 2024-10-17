b36d7766af039256b46e38dac4a71f7d47839e19
jonbmartin <jon.bach.martin@gmail.com>
2024-10-17 14:30:28 -0500

    print all losses

--- loss_functions.py
+++ loss_functions.py
@@ -290,6 +290,8 @@ def hypo_weight_loss(model_output):
 
 def image_hypernetwork_img_domain_loss(mask, kl, fw, model_output, gt):
     print(f'Hypo weight loss = {fw * hypo_weight_loss(model_output)}')
+    print(f'Latent loss = {kl * latent_loss(model_output)}')
+    print(f'Img loss = {ift_image_mse(mask, model_output, gt)['img_loss']}')
     return {'img_loss': ift_image_mse(mask, model_output, gt)['img_loss'],
             'latent_loss': kl * latent_loss(model_output),
             'hypo_weight_loss': fw * hypo_weight_loss(model_output)}
