4b8b395f54499072ed422267a9e117a93cfd3b3e
jonbmartin <jon.bach.martin@gmail.com>
2024-10-21 14:15:09 -0500

    correct fourier features scale

--- experiment_scripts/train_mri_neural_process_ddp.py
+++ experiment_scripts/train_mri_neural_process_ddp.py
@@ -279,7 +279,7 @@ if __name__ == "__main__":
     # TODO: manually setting this to be the same as that inside main()
     # create the fourier feature transform to be used by ALL DDP processes 
     num_fourier_features = 512
-    fourier_features_scale = 10
+    fourier_features_scale = 16
     device = 1
     resume_from_save = False
     experiment_name = 'DDP_RESET_large_dataset_batchnorm_hyper'
