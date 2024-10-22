570273afe675c213ea0b549fb9f6c1d1b2f236e5
jonbmartin <jon.bach.martin@gmail.com>
2024-10-22 11:52:05 -0500

    update experiment

--- experiment_scripts/train_mri_neural_process_ddp_image_domain.py
+++ experiment_scripts/train_mri_neural_process_ddp_image_domain.py
@@ -282,7 +282,7 @@ if __name__ == "__main__":
     fourier_features_scale = 1
     device = 1
     resume_from_save = False
-    experiment_name = 'DDP_RESET_img_domain_hybridloss'
+    experiment_name = 'DDP_RESET_img_domain_1e-2reg'
 
     if resume_from_save:
         load_from_checkpoint_path = './logs/DDP/checkpoints/model_epoch_0030.pth'
