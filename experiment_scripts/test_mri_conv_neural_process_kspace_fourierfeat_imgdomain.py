12a45d5fda320b68fdd4dbbfaba2a42076c06371
jonbmartin <jon.bach.martin@gmail.com>
2024-10-24 09:30:05 -0500

    update test config

--- experiment_scripts/test_mri_conv_neural_process_kspace_fourierfeat_imgdomain.py
+++ experiment_scripts/test_mri_conv_neural_process_kspace_fourierfeat_imgdomain.py
@@ -158,12 +158,12 @@ elif config =='hyperopt_highfreq':
     w0=30
 elif config =='from_early_expt':
     # Notes: Biggest improvements came from adding more hypernetwork layers. "Best = 0.0005 for batchsize 8"
-    num_fourier_features = 512
+    num_fourier_features = 512 # 256 is best
     kl_weight = 2.78e-8 #optim # 0.1 in paper
-    fw_weight = 1e-6#1e-2 best for mse #1e-6#optim # 100 in paper
+    fw_weight = 1e-2#1e-2 best for mse #1e-6#optim # 100 in paper
     lr = 1e-5#5.e-5 best for mse
-    fourier_features_scale = 1 # Best = 1! not 16
-    latent_dim = 2 # best = 256
+    fourier_features_scale = 1 # best = 1! not 16
+    latent_dim = 256 # best = 256
     hidden_features_hyper = 256 #256 # best = 256
     hidden_layers_hyper = 5 # try just 1. 3 gave 0.0011 after 15 epochs. 5 gave 0.0004- was best!! 
     hidden_layers = 6
@@ -172,6 +172,7 @@ elif config =='from_early_expt':
     conv_kernel_size = 5
     num_conv_res_blocks= 3 # go back to orig paper, was 3
     w0=30
+    dropout = 0.0
 
 device = 'cuda:5'
 
