1423c3ffafa9c61d8304cc1adb20fc5983fab2e8
jonbmartin <jon.bach.martin@gmail.com>
2024-10-25 09:50:52 -0500

    new hyperopt db

--- experiment_scripts/hyperopt_mri_neural_process.py
+++ experiment_scripts/hyperopt_mri_neural_process.py
@@ -123,7 +123,7 @@ def objective(trial, device_id):
 if __name__ == "__main__":
     study = optuna.load_study(
         storage = "sqlite:///db.sqlite3_test_img_domain",
-        study_name = 'hyperopt_img_domain_trial2')
+        study_name = 'hyperopt_img_domain_trial3')
     
     p = configargparse.ArgumentParser()
     p.add('-d', '--device_id', required=True, help='CUDA device ID.')
