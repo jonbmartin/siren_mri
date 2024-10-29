745af2eab926e49959d790dc48213d409d5b325e
jonbmartin <jon.bach.martin@gmail.com>
2024-10-29 11:26:26 -0500

    update

--- modules.py
+++ modules.py
@@ -36,7 +36,39 @@ class Sine(nn.Module):
         # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
         #return torch.sin(30 * input)
         return torch.sin(self.w0 * input)
+
+class RealGaborLayer(nn.Module):
+    '''
+        Implicit representation with Gabor nonlinearity
+        
+        Inputs;
+            in_features: Input features
+            out_features; Output features
+            bias: if True, enable bias for the linear operation
+            is_first: Legacy SIREN parameter
+            omega_0: Legacy SIREN parameter
+            omega: Frequency of Gabor sinusoid term
+            scale: Scaling of Gabor Gaussian term
+    '''
     
+    def __init__(self, in_features, out_features, bias=True,
+                 is_first=False, omega0=10.0, sigma0=10.0,
+                 trainable=False):
+        super().__init__()
+        self.omega_0 = omega0
+        self.scale_0 = sigma0
+        self.is_first = is_first
+        
+        self.in_features = in_features
+        
+        self.freqs = nn.Linear(in_features, out_features, bias=bias)
+        self.scale = nn.Linear(in_features, out_features, bias=bias)
+        
+    def forward(self, input):
+        omega = self.omega_0 * self.freqs(input)
+        scale = self.scale(input) * self.scale_0
+        
+        return torch.cos(omega)*torch.exp(-(scale**2))
 
 class ComplexGaborLayer2D(nn.Module):
     '''
@@ -124,8 +156,10 @@ class FCBlock(MetaModule):
                          'softplus':(nn.Softplus(), init_weights_normal, None),
                          'elu':(nn.ELU(inplace=True), init_weights_elu, None),
                          'swish':(nn.SiLU(inplace=True),init_weights_normal, None),
+                         'real_1d_gabor': (RealGaborLayer(in_features=hidden_features,out_features=hidden_features,
+                                                                omega0=30, sigma0=5.5), init_weights_normal,None),
                          'complex_2d_gabor':(ComplexGaborLayer2D(in_features=hidden_features,out_features=hidden_features,
-                                                                omega0=30, sigma0=5.5), init_weights_normal,None)}
+                                                                omega0=30, sigma0=5.5), init_weights_normal,None),}
 
         nl, nl_weight_init, first_layer_init = nls_and_inits[nonlinearity]
 
