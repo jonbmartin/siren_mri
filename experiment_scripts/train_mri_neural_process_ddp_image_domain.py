a52d11b17ea51cc980463e7a92327bc21a7ccaa6
jonbmartin <jon.bach.martin@gmail.com>
2024-10-30 10:10:16 -0500

    try full-size recon

--- experiment_scripts/train_mri_neural_process_ddp_image_domain.py
+++ experiment_scripts/train_mri_neural_process_ddp_image_domain.py
@@ -209,7 +209,7 @@ def main(rank, world_size, total_epochs, save_every, load_from_checkpoint_path, 
         w0=30
         dropout = 0.0
 
-    image_resolution = (128, 128)
+    image_resolution = (256, 256)
     use_fourier_features = True
     img_dataset = dataio.FastMRIBrainImageKspaceEncode(split='train', downsampled=True, image_resolution=image_resolution)
     #img_dataset = dataio.FastMRIBrain(split='train', downsampled=True, image_resolution=image_resolution)
--- loss_functions.py
+++ loss_functions.py
@@ -146,7 +146,7 @@ def image_mse_dc_loss(mask, model_output, gt, high_freq=False):
     # print(f'img dc loss = {dc_loss}')
     # print(f'img FD loss = {fd_loss}')
 
-    dimension_weight = 1/(128*128)
+    dimension_weight = 1/(256*256)
     # DC was /200 before.
     if mask is None:
         return {'img_loss': dimension_weight*(kspace_loss+dc_loss/5000+fd_loss/2)}
