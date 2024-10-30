e9f83d4eee77aa9f6e579b45f06df21fae7d9dcc
jonbmartin <jon.bach.martin@gmail.com>
2024-10-30 13:34:54 -0500

    make smaller

--- experiment_scripts/train_mri_neural_process_ddp_image_domain.py
+++ experiment_scripts/train_mri_neural_process_ddp_image_domain.py
@@ -193,12 +193,12 @@ def main(rank, world_size, total_epochs, save_every, load_from_checkpoint_path, 
         dropout = 0.0
     elif config =='hyperopt_w_autom':
         # Notes: Biggest improvements came from adding more hypernetwork layers. "Best = 0.0005 for batchsize 8"
-        num_fourier_features = 575 # 256 is best
+        num_fourier_features = 512 # 256 is best
         kl_weight = 1.25e-4 #optim # 0.1 in paper
         fw_weight = 7.6e-7#1e-2 best for mse #1e-6#optim # 100 in paper
         lr = 4.3e-6#5.e-5 best for mse
         fourier_features_scale = 5.5 # best = 1! not 16
-        latent_dim = 512 # best = 256
+        latent_dim = 256 # best = 256
         hidden_features_hyper = 128 #256 # best = 256
         hidden_layers_hyper = 1 # try just 1. 3 gave 0.0011 after 15 epochs. 5 gave 0.0004- was best!! 
         hidden_layers = 3
