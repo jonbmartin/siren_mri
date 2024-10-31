fbbb4eb2a7761ba256ff5adb35315ccdf4d2335b
jonbmartin <jon.bach.martin@gmail.com>
2024-10-30 22:47:28 -0500

    update

--- meta_modules.py
+++ meta_modules.py
@@ -216,10 +216,11 @@ class ConvolutionalNeuralProcessImplicit2DHypernetFourierFeatures(nn.Module):
             img_complex = model_input['img_sparse']
             print(np.shape(img_complex))
             img_complex = img_complex[:,0,:,:] + 1j* img_complex[:,1,:,:]
-            img_complex = torch.fft.ifft2(img_complex)
+            img_complex = torch.abs(torch.fft.ifft2(img_complex))
+            print(np.shape(img_complex))
             #img_complex = torch.stack((torch.real(img_complex),torch.imag(img_complex)),dim=1)
             #print(np.shape(img_complex))
-            embedding = self.encoder(torch.abs(img_complex))
+            embedding = self.encoder(img_complex)
             #embedding = self.encoder(model_input['img_sparse'])
         else:
             embedding = model_input['embedding']
